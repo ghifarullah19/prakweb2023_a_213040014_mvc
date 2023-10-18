196b80866f432e7f46fab812dab562213f10415c
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-10-18 10:39:02 +0700

    Commit Pertama

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2023_a_213040014_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
+
+?>
